9b66bec384d9c7eb22cdc281763f3c27313c2e2d
shb011 <sazzad.bhuiyan011@gmail.com>
2020-11-12 22:29:33 +0600

    update up to 12.11.2020

---
+++ backend/data.js
@@ -0,0 +1,44 @@
+export default{
+    products: [
+        {
+            _id:'1',
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/s2.jpg',
+            price: 1450,
+            brand: 'Top-Ten',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            _id:'2',
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/s2.jpg',
+            price: 1450,
+            brand: 'Top-Ten',
+            rating: 4.5,
+            numReviews: 9
+        },
+        {
+            _id:'3',
+            name: 'Pants',
+            category: 'Pants',
+            image: '/images/s2.jpg',
+            price: 1999,
+            brand: 'Top-Ten',
+            rating: 4.5,
+            numReviews: 8
+        },
+        {
+            _id:'4',
+            name: 'Gabardine pants',
+            category: 'Pants',
+            image: '/images/s2.jpg',
+            price: 1299,
+            brand: 'Top-Ten',
+            rating: 4,
+            numReviews: 10
+        },   
+    ]
+}
